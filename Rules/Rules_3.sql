084f9e0306409b3430306754333ac7348aa470d4
Gedemon <gedemon@free.fr>
2016-11-21 22:11:26 +0100

    - add import feature option to Largest Earth - changed Scythia starting position on Greatest Earth Map - changed Arabia and Greece (Gorgo) starting positions on Play Europe Again - add testing values for border growth modifiers with civics

--- Rules/Rules_3.sql
+++ Rules/Rules_3.sql
@@ -11,15 +11,38 @@
 
 INSERT OR REPLACE INTO GlobalParameters (Name, Value) VALUES ('YNAMP_RULESET', '3');
 
+-----------------------------------------------
 -- More moves on roads
+-----------------------------------------------
 UPDATE Routes SET MovementCost = 0.75 WHERE RouteType="ROUTE_ANCIENT_ROAD"; 
 UPDATE Routes SET MovementCost = 0.50 WHERE RouteType="ROUTE_MEDIEVAL_ROAD"; 
 UPDATE Routes SET MovementCost = 0.33 WHERE RouteType="ROUTE_INDUSTRIAL_ROAD"; 
 UPDATE Routes SET MovementCost = 0.25 WHERE RouteType="ROUTE_MODERN_ROAD"; 
 
+-----------------------------------------------
 -- Double movement on Ocean
+-----------------------------------------------
 UPDATE Terrains SET MovementCost = 2 WHERE TerrainType="TERRAIN_COAST"; 
 UPDATE GlobalParameters SET Value = Value * 2 WHERE Name ='MOVEMENT_WHILE_EMBARKED_BASE';
 UPDATE Units SET BaseMoves = BaseMoves * 2 WHERE Domain = 'DOMAIN_SEA';
 
+-----------------------------------------------
+-- Border Growth
+-----------------------------------------------
+
+UPDATE Terrains SET InfluenceCost = 5 WHERE TerrainType="TERRAIN_COAST";
+UPDATE Terrains SET InfluenceCost = 10 WHERE TerrainType="TERRAIN_OCEAN";
+
+UPDATE ModifierArguments SET Value = 50 WHERE ModifierId="RELIGIOUS_SETTLEMENTS_CULTUREBORDER";
+
+INSERT INTO Modifiers (ModifierId, ModifierType) VALUES ('ADJUST_BORDER_EXPANSION', 'MODIFIER_ALL_CITIES_CULTURE_BORDER_EXPANSION');
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES ('ADJUST_BORDER_EXPANSION', 'Amount', '100');
+
+INSERT INTO CivicModifiers (CivicType, ModifierId) VALUES ('CIVIC_CODE_OF_LAWS', 'ADJUST_BORDER_EXPANSION');
+INSERT INTO CivicModifiers (CivicType, ModifierId) VALUES ('CIVIC_CRAFTSMANSHIP', 'ADJUST_BORDER_EXPANSION');
+INSERT INTO CivicModifiers (CivicType, ModifierId) VALUES ('CIVIC_FOREIGN_TRADE', 'ADJUST_BORDER_EXPANSION');
+INSERT INTO CivicModifiers (CivicType, ModifierId) VALUES ('CIVIC_MILITARY_TRADITION', 'ADJUST_BORDER_EXPANSION');
+INSERT INTO CivicModifiers (CivicType, ModifierId) VALUES ('CIVIC_STATE_WORKFORCE', 'ADJUST_BORDER_EXPANSION');
+INSERT INTO CivicModifiers (CivicType, ModifierId) VALUES ('CIVIC_EARLY_EMPIRE', 'ADJUST_BORDER_EXPANSION');
+INSERT INTO CivicModifiers (CivicType, ModifierId) VALUES ('CIVIC_MYSTICISM', 'ADJUST_BORDER_EXPANSION');
 
