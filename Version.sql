8ea4fe86d5ae8b9477453f99a595363c1f680a9f
Gedemon <gedemon@free.fr>
2019-02-13 22:42:23 +0100

    - add TSL for Gathering Storm Civilizations thanks to SeelingCat - modifications to Giant Earth map based on suggestions in the map's thread on CFC

--- Version.sql
+++ Version.sql
@@ -4,4 +4,4 @@
 	
 */
 
-INSERT OR REPLACE INTO GlobalParameters (Name, Value) VALUES ('YNAMP_VERSION', 'Beta .7.3.5');
+INSERT OR REPLACE INTO GlobalParameters (Name, Value) VALUES ('YNAMP_VERSION', 'Beta .7.3.6');
